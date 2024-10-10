5b3ce7f4a3c342a56ecf64517684e1208736408a
jcbarbaf <jcbarbaf@gmail.com>
2024-10-10 14:57:50 +0200

    openai service

--- src/query-zacatrus.js
+++ src/query-zacatrus.js
@@ -1,18 +1,18 @@
+require('dotenv').config()
 const { ChromaClient } = require('chromadb')
 const chromaClient = new ChromaClient()
-const TelegramBot = require('node-telegram-bot-api');
+const TelegramBot = require('node-telegram-bot-api')
 const OpenAIService = require('./services/openai-service')
 
 class Zacatrus {
   constructor() {
-    this.prompt = "Busco un juego de cartas para niños de menos de 15 años"
     this.OpenAIService = null;
     this.token = process.env.TELEGRAM_BOT_TOKEN;
     this.bot = new TelegramBot(this.token, { polling: true }); 
 
-    this.bot.onText(/\/busca/, async (msg) => {
+    this.bot.onText(/\/bot/, async (msg) => {
       this.chatId = msg.chat.id; 
-      const message = msg.text.replace('/busca','')
+      const message = msg.text.replace('/bot','')
       await this.search(message);
     })
   }
@@ -22,26 +22,74 @@ class Zacatrus {
     const chromadbCollection = await chromaClient.getOrCreateCollection({ name: 'boardgames' })
     this.sendMessage("Estoy consultando la base de datos")
 
-    const object = await this.OpenAIService.extractKeywords(this.prompt)
+    // const result = await chromadbCollection.query({
+    //   nResults: 10,
+    //   queryTexts: [message]
+    // })
 
-    const result = await chromadbCollection.query({
-      nResults: 10,
-      queryTexts: [message]
-    })
+    // const elements = result.documents[0].map((_, i) => {
+    //   const element = {}
 
-    const elements = result.documents[0].map((_, i) => {
-      const element = {}
+    //   Object.entries(result.metadatas[0][i]).forEach(([key, value]) => {
+    //     element[key] = value
+    //   })
 
-      Object.entries(result.metadatas[0][i]).forEach(([key, value]) => {
-        element[key] = value
-      })
+    //   return element
+    // })
 
-      return element
-    })
 
-    const prompt = `${this.prompt} ${JSON.stringify(elements)}`
-    const answer = await this.OpenAIService.filterData(prompt)
+    // const prompt = `${this.prompt} ${JSON.stringify(elements)}`
+    const prompt = `${message}`
+    await this.OpenAIService.createThread()
+    await this.OpenAIService.setAssistant(process.env.ASSISTANT_ID)
+    await this.OpenAIService.createMessage(prompt)
+    await this.OpenAIService.runStatus()
 
+    const tools = await this.OpenAIService.getTools()
+
+    if (tools) {
+
+      const toolsOutputs = []
+    
+      for (const tool of tools) {
+        const data = JSON.parse(tool.function.arguments)
+    
+        if (tool.function.name === 'recommend_board_game') {
+
+          const count = await chromadbCollection.count()
+
+          const result = await chromadbCollection.query({
+            nResults: count,
+            queryTexts: ['']
+          })
+
+
+          const names = result.metadatas[0].map((metadata) => metadata.name);
+          const prompt = `Recomiendamé juegos parecidos en jugabilidad a ${data.gameName}  que aparezcan en la siguiente lista: ${names.join(',')}`
+          
+          this.sendMessage("Elaborando la respuesta...")
+
+          const answer = await this.OpenAIService.recommendDataFromtList(prompt)
+
+          const filteredResults = result.metadatas[0].filter(metadata =>
+            answer.recommendations.includes(metadata.name)
+          );
+
+          const output = {
+            answer: filteredResults
+          }
+
+          toolsOutputs.push({
+            tool_call_id: tool.id,
+            output: JSON.stringify(output)
+          })
+        }
+      }
+    
+      await this.OpenAIService.submitToolOutputs(toolsOutputs)
+    }
+
+    const answer = await this.OpenAIService.getAnswer()
     this.sendMessage(answer)
   }
 
--- src/services/openai-service.js
+++ src/services/openai-service.js
@@ -1,4 +1,3 @@
-require('dotenv').config();
 const OpenAI = require('openai')
 const { zodResponseFormat } = require('openai/helpers/zod')
 const { z } = require('zod')
@@ -8,6 +7,140 @@ module.exports = class OpenAIService {
     this.openai = new OpenAI({
       apiKey: process.env.OPENAI_API_KEY
     })
+    this.assistantEndpoint = null
+    this.threadId = null
+    this.messages = null
+    this.answer = null
+    this.tools = null
+  }
+
+  async getAnswer () {
+    return this.answer
+  }
+
+  async getTools () {
+    return this.tools
+  }
+
+
+  async setAssistant (assistantEndpoint) {
+    this.assistantEndpoint = assistantEndpoint
+  }
+
+  async createThread () {
+    try {
+      const thread = await this.openai.beta.threads.create()
+      this.threadId = thread.id
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  setThread (theadId) {
+    this.threadId = theadId
+  }
+
+  async createMessage (prompt) {
+    try {
+      await this.openai.beta.threads.messages.create(
+        this.threadId,
+        {
+          role: 'user',
+          content: prompt
+        }
+      )
+
+      this.run = await this.openai.beta.threads.runs.createAndPoll(
+        this.threadId,
+        {
+          assistant_id: this.assistantEndpoint
+        }
+      )
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  async runStatus () {
+    try {
+      
+      if (this.run.status === 'completed') {
+        const messages = await this.openai.beta.threads.messages.list(this.run.thread_id)
+        this.messages = messages.data
+        this.answer = this.messages[0].content[0].text.value
+        return
+      }
+
+      if (
+        this.run.required_action &&
+        this.run.required_action.submit_tool_outputs &&
+        this.run.required_action.submit_tool_outputs.tool_calls
+      ) {
+        this.tools = this.run.required_action.submit_tool_outputs.tool_calls
+        return
+      }
+
+      if (this.run.status === 'queued' || this.run.status === 'in_progress') {
+        await this.sleep(2000)
+
+        this.run = await this.openai.beta.threads.runs.retrieve(
+          this.threadId,
+          this.run.id
+        )
+
+        await this.runStatus()
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  async submitToolOutputs (toolOutputs) {
+    try {
+      this.run = await this.openai.beta.threads.runs.submitToolOutputs(
+        this.threadId,
+        this.run.id,
+        { tool_outputs: toolOutputs }
+      )
+
+      await this.runStatus()
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  async analyzeImages (images, prompt) {
+    const content = [{ type: 'text', text: `${prompt}` }]
+
+    for (const image of images) {
+      content.push({
+        type: 'image_url',
+        image_url: {
+          url: `data:image/webp;base64,${image}`,
+          detail: 'high'
+        }
+      })
+    }
+
+    const response = await this.openai.chat.completions.create({
+      model: 'gpt-4o-mini',
+      messages: [
+        {
+          role: 'user',
+          content
+        }
+      ],
+      temperature: 1,
+      max_tokens: 2048,
+      top_p: 1,
+      frequency_penalty: 0,
+      presence_penalty: 0,
+      response_format: {
+        type: 'text'
+      }
+    })
+
+    return response.choices[0].message.content
   }
 
   filterData = async (prompt, data) => {
@@ -20,7 +153,8 @@ module.exports = class OpenAIService {
             content: [
               {
                 type: 'text',
-                text: `Ante la pregunta del usuario, proporciona una respuesta humana que contenga los elementos que más se acerquen a la búsqueda del usuario y contengan la url de cada elemento. Si no encuentras nada responde "No he encontrado nada"`
+                text: `Ante la pregunta del usuario, proporciona una respuesta humana que contenga los elementos que más se acerquen a la búsqueda del usuario y contengan la url de cada elemento. Si no encuentras nada responde "No he encontrado nada".
+                Tu respuesta debe contener etiquetas HTML para que sea visualmente atractiva.`
               }
             ]
           },
@@ -29,7 +163,7 @@ module.exports = class OpenAIService {
             content: [
               {
                 type: 'text',
-                text: `${prompt}`
+                text: `El usuario ha preguntado lo siguiente: ${prompt}, y estos son los datos que se han obtenido: ${data}`
               }
             ]
           }
@@ -45,6 +179,53 @@ module.exports = class OpenAIService {
       })
 
       return response.choices[0].message.content
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  recommendDataFromtList = async (prompt) => {
+
+    const Recommendation = z.object({
+      recommendations: z.array(z.string())
+    })
+
+    try {
+      const response = await this.openai.beta.chat.completions.parse({
+        model: 'gpt-4o-mini-2024-07-18',
+        messages: [
+          {
+            role: 'system',
+            content: [
+              {
+                type: 'text',
+                text: `Eres un recomendador de productos similares. Devuelve una lista de cinco nombres de productos a partir de la que el usuario te ofrezca. 
+                Los nombre que me devuelvas deben estar escritos exactamente igual que como aparecen en la lista que te haya dado el usuario. Elige diversidad, 
+                no recomiendes diferentes versiones de un mismo juego, y en la medida de lo posible evita expansiones del mismo juego planteado por el usuario`
+              }
+            ]
+          },
+          {
+            role: 'user',
+            content: [
+              {
+                type: 'text',
+                text: `${prompt}`
+              }
+            ]
+          }
+        ],
+        temperature: 1,
+        max_tokens: 16384,
+        top_p: 1,
+        frequency_penalty: 0,
+        presence_penalty: 0,
+        response_format: zodResponseFormat(Recommendation, 'recommendations')
+      })
+
+      const recommendations = response.choices[0].message.parsed
+      return recommendations
+
     } catch (error) {
       console.log(error)
     }
@@ -86,10 +267,26 @@ module.exports = class OpenAIService {
         response_format: zodResponseFormat(Keywords, 'keywords')
       })
 
-      const keywords= response.choices[0].message.parsed
+      const keywords = response.choices[0].message.parsed
       return keywords
     } catch (error) {
       console.log(error)
     }
+  }
+
+  extractCode (response) {
+    const regex = /```([^```]+)```/g
+    const matches = response.match(regex)
+
+    if (matches && matches.length > 0) {
+      response = matches[0].replace(/```/g, '')
+      return response
+    } else {
+      return response
+    }
+  }
+
+  sleep (ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms))
   }
 }
